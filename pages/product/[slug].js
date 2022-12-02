068a1c71a4950a776e59376eb9ec6c2d33d03ca1
Jordan Epps <jordanepps@gmail.com>
2022-12-02 08:36:39 -0500

    Added cart component styling and logic for items and subtotal

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { client, urlFor } from '../../lib/client';
 import { Product } from '../../components';
+import { useStateContext } from '../../context/StateContext';
 
 import {
   AiOutlineMinus,
@@ -12,6 +13,7 @@ import {
 const ProductDetails = ({ product, products }) => {
   const [index, setIndex] = useState(0);
   const { image, name, details, price } = product;
+  const { decQty, incQty, quantity, onAdd } = useStateContext();
 
   return (
     <div>
@@ -55,19 +57,25 @@ const ProductDetails = ({ product, products }) => {
           <div className="quantity">
             <h3>Quantity</h3>
             <p className="quantity-desc">
-              <span className="minus" onClick="">
+              <span className="minus" onClick={decQty}>
                 <AiOutlineMinus />
               </span>
               <span className="num" onClick="">
-                0
+                {quantity}
               </span>
-              <span className="plus" onClick="">
+              <span className="plus" onClick={incQty}>
                 <AiOutlinePlus />
               </span>
             </p>
           </div>
           <div className="buttons">
-            <button type="button" className="add-to-cart" onClick="">
+            <button
+              type="button"
+              className="add-to-cart"
+              onClick={() => {
+                onAdd(product, quantity);
+              }}
+            >
               Add to Cart
             </button>
             <button type="button" className="buy-now" onClick="">
