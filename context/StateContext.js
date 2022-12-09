fbc147e0b90e1b2ccf5a8c53bb9fa3762e5542c3
Jordan Epps <jordanepps@gmail.com>
2022-12-09 09:25:19 -0500

    Fixed bug where cart items would change order on inc/dec quantity adjustment

--- context/StateContext.js
+++ context/StateContext.js
@@ -53,25 +53,44 @@ export const StateContext = ({ children }) => {
     //TODO: Fix bug where cart order changes on inc/dec
     foundProduct = cartItems.find((item) => item._id === id);
     index = cartItems.findIndex((product) => product._id === id);
-    const newCartItems = cartItems.filter((item) => item._id !== id);
-    if (value === 'inc') {
-      setCartItems([
-        ...newCartItems,
-        { ...foundProduct, quantity: foundProduct.quantity + 1 },
-      ]);
-      setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price);
-      setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + 1);
-    } else if (value === 'dec') {
-      if (foundProduct.quantity > 1) {
-        setCartItems([
-          ...newCartItems,
-          { ...foundProduct, quantity: foundProduct.quantity - 1 },
-        ]);
+    // const newCartItems = cartItems.filter((item) => item._id !== id);
+
+
+    const editedCart = cartItems.map(({...item}) => {
+      if (foundProduct._id === item._id && value === 'inc') {
+        setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price);
+        setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + 1);
+        return {...item, quantity: item.quantity + 1}
+      } else if (foundProduct._id === item._id && value === 'dec') {
         setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price);
         setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - 1);
+        return {...item, quantity: item.quantity - 1}
+      } else {
+        return {...item}
       }
-      //TODO: Handle logic to remove item once dec past 1
-    }
+    })
+
+    setCartItems(editedCart);
+  
+
+    // if (value === 'inc') {
+    //   setCartItems([
+    //     ...newCartItems,
+    //     { ...foundProduct, quantity: foundProduct.quantity + 1 },
+    //   ]);
+    //   setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price);
+    //   setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + 1);
+    // } else if (value === 'dec') {
+    //   if (foundProduct.quantity > 1) {
+    //     setCartItems([
+    //       ...newCartItems,
+    //       { ...foundProduct, quantity: foundProduct.quantity - 1 },
+    //     ]);
+    //     setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price);
+    //     setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - 1);
+    //   }
+    // }
+
   };
 
   const incQty = () => {
